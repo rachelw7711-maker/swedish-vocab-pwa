cddc1283d7b4462ea74ec344513f7daca160899e
huijingwang <huijingwang@users.noreply.github.com>
2026-07-06 08:13:07 +0200

    Fix Shadowing app cache version

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -37,6 +37,7 @@ const checks = [
       'href="/icons/favicon.ico?v=22"',
       'href="/icons/apple-touch-icon.png?v=22"',
       'src="/icons/app-icon.png?v=22"',
+      'src="/app.js?v=107"',
       'rel="shortcut icon"',
       'rel="apple-touch-icon"',
       'type="image/png"',
